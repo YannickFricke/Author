f038d133516fa0148626dbb542add4b7ddc3a751
Yannick Fricke <yannickfricke@googlemail.com>
2021-10-09 10:36:07 +0200

    Added documentation to the Author module

--- lib/author.ex
+++ lib/author.ex
@@ -1,5 +1,40 @@
 defmodule Author do
-  @moduledoc """
-  Documentation for `Author`.
+  @moduledoc ~S"""
+  `Author` is a set of functions which can be used by server-side elixir applications
+  to integrate authentication and authorization into the server.
+
+  In the future different authentication and authorization methods will be supported.
+  This will be
+  - Basic HTTP authentication
+  - OAuth
+  - OpenID Connect (OIDC)
+
+  ## Roadmap
+
+  - Implement entities
+    - Add the user entity
+  - Implement Basic Authentication
+  - Implement OAuth
+    - Add the scope entity
+    - Add the token entity
+    - Add the protected resource entity
+  - Implement a DSL for protected resources
+
+  ## Installation
+
+  When available in [hex](https://hexdocs.pm/author) the package can be installed with:
+  ```elixir
+  def deps(), do: [
+    {:author, "~> 0.0.0"}
+  ]
+  ```
+
+  ## Usage
+
+  ## License
+
+  `Author` is licensed under the MIT license.
   """
+
+  # TODO: Add "Usage" chapter
 end
