254b2d8770de9ea6a0b0ff07bfe7ee749f1dfdf6
Yannick Fricke <yannickfricke@googlemail.com>
2021-10-10 19:42:57 +0200

    Some style fixes to the application module

--- lib/author/application.ex
+++ lib/author/application.ex
@@ -1,16 +1,11 @@
 defmodule Author.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
-  @impl true
+  @impl Application
   def start(_type, _args) do
-    children = [
-      # Starts a worker by calling: Author.Worker.start_link(arg)
-      # {Author.Worker, arg}
-    ]
+    children = []
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
